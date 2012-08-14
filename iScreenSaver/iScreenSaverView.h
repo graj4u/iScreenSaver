86f32ff7f2c6e75d8398ce0e5c76c82bce099139
Gaurav <gauravraj@me.com>
2012-08-14 09:54:26 +0530

    Initial Commit

---
+++ iScreenSaver/iScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  iScreenSaverView.h
+//  iScreenSaver
+//
+//  Created by Gaurav Raj on 8/14/12.
+//  Copyright (c) 2012 Gaurav Raj. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface iScreenSaverView : ScreenSaverView
+
+@end
---
+++ iScreenSaver/iScreenSaverView.m
@@ -0,0 +1,52 @@
+//
+//  iScreenSaverView.m
+//  iScreenSaver
+//
+//  Created by Gaurav Raj on 8/14/12.
+//  Copyright (c) 2012 Gaurav Raj. All rights reserved.
+//
+
+#import "iScreenSaverView.h"
+
+@implementation iScreenSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
